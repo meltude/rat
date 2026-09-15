0f990fb87e641627f5089e3f9b98b475a8be752f
Dmytro Yatsenko <dima0102yacenko@gmail.com>
2026-09-15 19:19:11 +0200

    tcp stream for text, tcp stream for image; new channel for img logic

--- server/src/server.rs
+++ server/src/server.rs
@@ -11,19 +11,18 @@ pub enum ClientEvent {
 pub struct ClientHandle {
     pub app_sender: mpsc::Sender<Vec<u8>>,   
     pub app_receiver: mpsc::Receiver<ClientEvent>, 
+    pub app_img_receiver: mpsc::Receiver<ClientEvent>,
 }
 
-pub fn spawn_client(addr: &str) -> ClientHandle {
+pub async fn spawn_client(addr1: &str, addr2: &str) -> Result<ClientHandle, io::Error> {
     let (app_sender, mut socket_receiver) = mpsc::channel::<Vec<u8>>(32);
     let (socket_sender, app_receiver) = mpsc::channel::<ClientEvent>(32);
-    let addr = addr.to_string();
+    let (socket_img_sender, app_img_receiver) = mpsc::channel::<ClientEvent>(32);
+
+    let listener = TcpListener::bind(addr1).await?;
+    let listener_img = TcpListener::bind(addr2).await?;
 
     tokio::spawn(async move {
-        let listener = match TcpListener::bind(&addr).await {
-            Ok(l) => l,
-            Err(_) => return,
-        };
-
         loop {
             let (socket, peer_addr) = match listener.accept().await {
                 Ok(pair) => pair,
@@ -55,5 +54,34 @@ pub fn spawn_client(addr: &str) -> ClientHandle {
         }
     });
 
-    ClientHandle { app_sender, app_receiver }
+    tokio::spawn(async move {
+        loop {
+            let (socket, peer_addr) = match listener_img.accept().await {
+                Ok(pair) => pair,
+                Err(_) => continue,
+            };
+            let _ = socket_img_sender.send(ClientEvent::Connected(peer_addr)).await;
+
+            let (mut rd, _wr) = io::split(socket);
+
+            let mut buf = [0u8; 1024];
+
+            loop {
+                tokio::select! {
+                    read_result = rd.read(&mut buf) => {
+                        match read_result {
+                            Ok(0) | Err(_) => break,
+                            Ok(n) => {
+                                let _ = socket_img_sender.send(ClientEvent::Data(buf[..n].to_vec())).await;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+    Ok(ClientHandle { 
+        app_sender, app_receiver, app_img_receiver 
+    })
 }
