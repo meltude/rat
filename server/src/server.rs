0ce5918cf63c0fa2b30180d17494097e8d2147cd
Dmytro Yatsenko <dima0102yacenko@gmail.com>
2026-09-18 16:04:04 +0200

    bytes is not incoming in TCP socket for image

--- server/src/server.rs
+++ server/src/server.rs
@@ -1,6 +1,6 @@
-use tokio::sync::mpsc;
-use tokio::net::TcpListener;
-use tokio::io::{self, AsyncWriteExt, AsyncReadExt};
+use tokio::sync::mpsc::{self, Receiver, Sender};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::io::{self, AsyncReadExt, AsyncWriteExt, ReadHalf};
 
 pub enum ClientEvent {
     Connected(std::net::SocketAddr),
@@ -22,6 +22,19 @@ pub async fn spawn_client(addr1: &str, addr2: &str) -> Result<ClientHandle, io::
     let listener = TcpListener::bind(addr1).await?;
     let listener_img = TcpListener::bind(addr2).await?;
 
+    socket_txt(listener, socket_sender, socket_receiver).await;
+    socket_img(listener_img, socket_img_sender).await;
+
+    Ok(ClientHandle { 
+        app_sender, app_receiver, app_img_receiver 
+    })
+}
+
+async fn socket_txt(
+    listener: TcpListener, 
+    socket_sender: Sender<ClientEvent>,
+    mut socket_receiver: Receiver<Vec<u8>>,
+) {
     tokio::spawn(async move {
         loop {
             let (socket, peer_addr) = match listener.accept().await {
@@ -53,35 +66,34 @@ pub async fn spawn_client(addr1: &str, addr2: &str) -> Result<ClientHandle, io::
             let _ = socket_sender.send(ClientEvent::Disconnected).await;
         }
     });
+}
 
+async fn socket_img(
+    listener: TcpListener,
+    socket_sender: Sender<ClientEvent>,
+) {
     tokio::spawn(async move {
         loop {
-            let (socket, peer_addr) = match listener_img.accept().await {
+            let (socket, peer_addr) = match listener.accept().await {
                 Ok(pair) => pair,
                 Err(_) => continue,
             };
-            let _ = socket_img_sender.send(ClientEvent::Connected(peer_addr)).await;
+            let _ = socket_sender.send(ClientEvent::Connected(peer_addr)).await;
 
-            let (mut rd, _wr) = io::split(socket);
+            let (mut rd, _) = io::split(socket);
 
             let mut buf = [0u8; 1024];
 
             loop {
-                tokio::select! {
-                    read_result = rd.read(&mut buf) => {
-                        match read_result {
-                            Ok(0) | Err(_) => break,
-                            Ok(n) => {
-                                let _ = socket_img_sender.send(ClientEvent::Data(buf[..n].to_vec())).await;
-                            }
-                        }
-                    }
+                let n = rd.read(&mut buf).await?;
+
+                if n == 0 {
+                    break;
                 }
+
+                let _ = socket_sender.send(ClientEvent::Data(buf[..n].to_vec())).await;
             }
         }
+        Ok::<_, io::Error>(())
     });
-
-    Ok(ClientHandle { 
-        app_sender, app_receiver, app_img_receiver 
-    })
 }
